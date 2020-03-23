45743ae9f8b7c4573c5f97489bff53f56f51250c
Hieu Ho <hihho@ucdavis.edu>
2020-03-23 11:37:33 -0700

    Added getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -7,4 +7,28 @@
 var getElementsByClassName = function(className
 ) {
   // your code here
+  // create an array for storing elements
+  let resultsArr = [];
+  // helper function
+  let helper = function(elements) {
+    if (elements.classList && elements.classList.contains(className)){
+      resultsArr.push(elements);
+    }
+    //check if childnode
+    if (elements.hasChildNodes()) {
+      // get the children
+      //if yes, go into childnode recursively
+      let children = elements.childNodes;
+      for (var i = 0; i < children.length; i++) {
+        //add to resultArr
+        helper(children[i]);
+      }
+    }
+  }
+  //run helper func through document.body
+  helper(document.body);
+
+
+  // return that array
+  return resultsArr;
 };
